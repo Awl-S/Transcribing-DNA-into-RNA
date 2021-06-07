c4811efc46e5bca77acdf708427eb26bfbf6e5c8
Твоя Булочка <85406297+Romb1k@users.noreply.github.com>
2021-06-08 01:10:35 +0300

    Transcribing DNA into RNA

---
+++ Transcribing DNA into RNA.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	string str; cin >> str;
+	replace(str.begin(), str.end(), 'T', 'U');	
+	cout << str << endl;
+
+}
